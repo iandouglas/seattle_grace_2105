0d27fe9ececee1f61c9adecb39198cc4ec818eed
ian douglas <ian.douglas@iandouglas.com>
2021-05-26 14:27:20 -0600

    iteration 1 complete with Megan

---
+++ spec/doctor_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require './lib/doctor'
+
+RSpec.describe Doctor do 
+  describe 'instantiation' do 
+    it 'exists' do
+      meredith = Doctor.new({
+        name: "Meredith Grey", 
+        specialty: "General Surgery", 
+        education: "Harvard University", 
+        salary: 100_000
+      })
+
+      expect(meredith).to be_a(Doctor)
+    end
+
+    it 'has attributes on instantiation' do 
+      meredith = Doctor.new({
+        name: "Meredith Grey", 
+        specialty: "General Surgery", 
+        education: "Harvard University", 
+        salary: 100_000
+      })
+
+      expect(meredith.name).to eq("Meredith Grey")
+      expect(meredith.specialty).to eq("General Surgery")
+      expect(meredith.education).to eq("Harvard University")
+      expect(meredith.salary).to eq(100_000)
+    end
+  end
+
+  describe 'methods' do
+  end
+end
